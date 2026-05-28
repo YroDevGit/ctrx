2522d9349d626e2eccde399bd71566cb06e71330
Tyrone <tyrone@clearmindai.com>
2026-05-28 09:04:02 +0800

    fe variable

--- app/php/core/partials/envloader.php
+++ app/php/core/partials/envloader.php
@@ -26,8 +26,22 @@ if (file_exists(".env")) {
     }
 }
 if (! defined("ctr_secure_key")) define("ctr_secure_key", "csrf_ctrsk_" . getenv('secure_key'));
-if(! function_exists("env")){
-    function env(string $key){
+if (! function_exists("env")) {
+    function env(string $key)
+    {
         return getenv($key);
+    }
+}
+
+if (! function_exists("variable")) {
+    function variable(string $key)
+    {
+        $view_config = file_get_contents("views/fe_config.json");
+        $view_config = json_decode($view_config, true);
+        if (isset($view_config['variable'])) {
+            $vr = $view_config['variable'];
+            return $vr[$key] ?? null;
+        }
+        return null;
     }
 }
