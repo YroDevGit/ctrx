18ad8004f5a5a50a582a181b337ff2d32ff4ffb6
Tyrone <tyrone@clearmindai.com>
2026-06-11 10:12:30 +0800

    db tools comment

--- app/config/db_tools.php
+++ app/config/db_tools.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/** This is only tested only in MariaDB mysql database
  * This file blocks users to use db tools
  * make a logic to filter users that can access db tool
  * /ctrxtools/db
